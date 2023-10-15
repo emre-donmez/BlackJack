6a13220ec061eb4259d09e6b5f117e99c45a1465
Emrecan <emrecandonmez.bt@gmail.com>
2023-10-15 13:18:16 +0300

    Program.cs has been restored.

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -2,12 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +17,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseSession();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
